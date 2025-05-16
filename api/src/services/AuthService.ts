36b14f42a13ff24b9bba7baca6e6d38f2bc79527
eduardocaas <eduardofc.dev@gmail.com>
2025-05-15 23:34:36 -0300

    Add method generateHash

--- api/src/services/AuthService.ts
+++ api/src/services/AuthService.ts
@@ -1,8 +1,7 @@
 import { Repository } from "typeorm";
 import { User } from "../models/User";
 import { IAuthService } from "./interfaces/IAuthService";
-import { SignupInputModel } from "../models/input/user/SignupInputModel";
-import { UserPartialViewModel } from "../models/view/user/UserPartialViewModel";
+import * as bcrypt from 'bcrypt';
 
 export class AuthService implements IAuthService {
   private readonly _repository: Repository<User>;
@@ -10,15 +9,8 @@ export class AuthService implements IAuthService {
   constructor(repository: Repository<User>) {
     this._repository = repository;
   }
-
-  async signup(input: SignupInputModel): Promise<UserPartialViewModel> {
-    if (!input?.name || input?.email || !input?.password) {
-      throw new Error("Campos obrigatórios: nome, email e senha");  
-    }
-    // TODO: bCrypt Password!
-    let user = new User(input.name, input.email, input.password);
-    let userSave = await this._repository.save(user);
-    return new UserPartialViewModel(userSave.id, userSave.email);
-
+  
+  generateHash(input: string): Promise<string> {
+    throw new Error("Method not implemented.");
   }
 }
--- api/src/services/interfaces/IAuthService.ts
+++ api/src/services/interfaces/IAuthService.ts
@@ -1,6 +1,3 @@
-import { SignupInputModel } from "../../models/input/user/SignupInputModel";
-import { UserPartialViewModel } from "../../models/view/user/UserPartialViewModel";
-
 export interface IAuthService {
-    signup(input: SignupInputModel): Promise<UserPartialViewModel>;
+    generateHash(input: string): Promise<string>;
 }
