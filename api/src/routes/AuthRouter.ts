fe8d87702216f74c609988d7632174bb33ab8b04
Eduardo <eduardofc.dev@gmail.com>
2025-05-17 20:20:51 -0300

    Add signup and signin routes mapping

--- api/src/routes/AuthRouter.ts
+++ api/src/routes/AuthRouter.ts
@@ -4,5 +4,8 @@ import { AuthController } from "../controllers/AuthController";
 export const authRoutes = (controller: AuthController) => {
   const router = Router();
 
+  router.post('/signup', controller.signUp);
+  router.post('/signin', controller.signIn);
+  
   return router;
 }
