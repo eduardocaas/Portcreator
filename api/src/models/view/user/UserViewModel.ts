7a2e9a8947221a18b0a0a11cfa49d9a870e9c2d6
eduardocaas <eduardofc.dev@gmail.com>
2025-05-19 12:03:08 -0300

    Add UserViewModel.ts

---
+++ api/src/models/view/user/UserViewModel.ts
@@ -0,0 +1,34 @@
+export class UserViewModel {
+
+  id: string;
+  name: string;
+  email: string;
+  location?: string;
+  description?: string;
+  goal?: string;
+  github?: string;
+  linkedin?: string;
+  imagePath?: string;
+
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    location?: string,
+    description?: string,
+    goal?: string,
+    github?: string,
+    linkedin?: string,
+    imagePath?: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.location = location;
+    this.description = description;
+    this.goal = goal;
+    this.github = github;
+    this.linkedin = linkedin;
+    this.imagePath = imagePath;
+  }
+
+}
