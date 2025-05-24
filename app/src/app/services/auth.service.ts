8340ddad7464d2c7d9eb3b61719cb8789cd968a6
Eduardo <eduardofc.dev@gmail.com>
2025-05-24 17:45:09 -0300

    Implemented methods for authentication

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -1,19 +1,47 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Credentials } from '../models/Credentials';
-import { AuthResponse } from '../models/responses/AuthResponse';
 import { environment } from '../../environments/environment';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { catchError, Observable, of, tap } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  jwtHelper: JwtHelperService = new JwtHelperService();
+
   constructor(private readonly _http: HttpClient) { }
 
-  authenticate(credentials: Credentials) {
-    return this._http.post<AuthResponse>(`${environment.apiUrl}/api/auth/signin`, credentials, {
-      observe: 'response'
-    });
+  authenticate(credentials: Credentials): Observable<boolean> {
+    return this._http.post<any>(`${environment.apiUrl}/api/auth/signin`, credentials, httpOptions)
+      .pipe(
+        tap((resp: any) => {
+          if (resp?.token) {
+            sessionStorage.setItem('token', resp.token);
+            return true;
+          }
+          else {
+            return false;
+          }
+
+        }),
+        catchError(error => of(false))
+      );
+  }
+
+  isAuthenticated(): boolean {
+    const token = sessionStorage.getItem('token');
+    if (token != null && token != undefined) {
+      return !this.jwtHelper.isTokenExpired(token);
+    }
+    return false
   }
 }
