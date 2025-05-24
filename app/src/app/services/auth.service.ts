8f61e8413dd0f825155c28889fbd7d8a22ded2d5
Eduardo <eduardofc.dev@gmail.com>
2025-05-24 01:01:21 -0300

    Implemented method authenticate

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Credentials } from '../models/Credentials';
+import { AuthResponse } from '../models/responses/AuthResponse';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private readonly _http: HttpClient) { }
+
+  authenticate(credentials: Credentials) {
+    return this._http.post<AuthResponse>(`${environment.apiUrl}/api/auth/signin`, credentials, {
+      observe: 'response'
+    });
+  }
 }
