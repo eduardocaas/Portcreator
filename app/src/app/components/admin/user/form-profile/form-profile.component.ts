9aedadb46dfc91ff1a23d2459b470e18713e040b
Eduardo Castro <eduardofc.dev@gmail.com>
2025-05-28 17:43:24 -0300

    Add UserUpdate model

--- app/src/app/components/admin/user/form-profile/form-profile.component.ts
+++ app/src/app/components/admin/user/form-profile/form-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserUpdate } from '../../../../models/admin/UserUpdate';
 
 @Component({
   selector: 'app-form-profile',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './form-profile.component.css'
 })
 export class FormProfileComponent {
-
+  user: UserUpdate = {
+    id: '',
+    name: '',
+    email: '',
+    location: '',
+    description: '',
+    goal: '',
+    github: '',
+    linkedin: ''
+  }
 }
---
+++ app/src/app/models/admin/UserUpdate.ts
@@ -0,0 +1,10 @@
+export interface UserUpdate {
+  id: string,
+  name: string,
+  email: string,
+  location?: string,
+  description?: string,
+  goal?: string,
+  github?: string,
+  linkedin?: string
+}
