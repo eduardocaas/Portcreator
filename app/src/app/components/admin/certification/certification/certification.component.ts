1f7044875a7a61aeab97c07b49701de14a1bbe62
eduardocaas <eduardofc.dev@gmail.com>
2025-06-02 11:48:12 -0300

    Add method get Certifications and Toast

--- app/src/app/components/admin/certification/certification/certification.component.ts
+++ app/src/app/components/admin/certification/certification/certification.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Toast } from 'bootstrap';
+import { CertificationPartial } from 'src/app/models/admin/CertificationPartial';
+import { UserMessage } from 'src/app/models/messages/UserMessage';
+import { CertificationService } from 'src/app/services/certification.service';
 
 @Component({
   selector: 'app-certification',
   templateUrl: './certification.component.html',
   styleUrl: './certification.component.css'
 })
-export class CertificationComponent {
+export class CertificationComponent implements OnInit {
+
+  constructor(private readonly _service: CertificationService) { }
+  toastMessage: string | null = null;
+  certifications: CertificationPartial[] = [];
+
+  ngOnInit(): void {
+    this.toastMessage = null;
+    this._service.getAll().subscribe({
+      next: (res) => {
+        this.certifications = res;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err);
+        this.toastMessage = "Falha ao carregar certificações";
+        this.showErrorToast();
+      },
+    });
+  }
+
+  showErrorToast() {
+    const toastElement = document.getElementById('errorToast');
+    if (toastElement) {
+      const toast = new Toast(toastElement);
+      toast.show();
+    }
+  }
+
 
 }
