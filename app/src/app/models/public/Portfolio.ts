e2f8ce0103c95afcb7162b708ee862464beef677
Eduardo <eduardofc.dev@gmail.com>
2025-06-22 22:12:25 -0300

    Change interface to class

--- app/src/app/models/public/Portfolio.ts
+++ app/src/app/models/public/Portfolio.ts
@@ -1,13 +1,25 @@
 import { Certification } from "../admin/Certification";
+import { User } from "../admin/UserUpdate";
 
-export interface Portfolio {
-  id: string,
-  name: string,
-  email?: string,
-  location?: string,
-  description?: string,
-  goal?: string,
-  github?: string,
-  linkedin?: string
+export class Portfolio {
+  id?: string | null;
+  name?: string;
+  email?: string | null;
+  location?: string;
+  description?: string;
+  goal?: string;
+  github?: string;
+  linkedin?: string;
   certifications?: Certification[];
+
+  setUser(user: User) {
+    this.id = user.id;
+    this.name = user.name;
+    this.email = user.email;
+    this.location = user.location;
+    this.description = user.description;
+    this.goal = user.goal;
+    this.github = user.github;
+    this.linkedin = user.linkedin;
+  }
 }
