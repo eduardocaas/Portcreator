b90050062d82e4c2771f4504d0e5745c25516795
eduardocaas <eduardofc.dev@gmail.com>
2025-07-29 23:10:44 -0300

    Add Environment example file

---
+++ app/src/environments/environment.example.ts
@@ -0,0 +1,11 @@
+/*export*/ const environment = {
+  apiUrl: 'URL_API',
+  firebase: {
+    "projectId":"ID_PROJECT_FIREBASE",
+    "appId":"ID_APP_FIREBASE",
+    "storageBucket":"FIREBASE_STORAGE_BUCKET",
+    "apiKey":"FIREBASE_API_KEY",
+    "authDomain":"FIREBASE_AUTH_DOMAIN",
+    "messagingSenderId":"MESSAGE_SENDER_ID"
+  }
+}
