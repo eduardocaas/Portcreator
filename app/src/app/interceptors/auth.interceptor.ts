82ff3565ae23b92b0cd93b96d8df12abcd36f820
eduardocaas <eduardofc.dev@gmail.com>
2025-07-31 21:43:21 -0300

    Add Invalid token redirect

--- app/src/app/interceptors/auth.interceptor.ts
+++ app/src/app/interceptors/auth.interceptor.ts
@@ -1,15 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  let token = sessionStorage.getItem('token');
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const token = sessionStorage.getItem('token');
+
+  let authReq = req;
+
   if (token) {
-    const cloneReq = req.clone({
+    authReq = req.clone({
       setHeaders: {
         "Token": token
       }
     });
-    return next(cloneReq);
-  } else {
-    return next(req);
   }
+
+  return next(authReq).pipe(
+    catchError((error: any) => {
+      /* Se der erro 401, logout e login */
+      if (error instanceof HttpErrorResponse && error.status === 401) {
+        authService.logout();
+        router.navigate(['/signin']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
